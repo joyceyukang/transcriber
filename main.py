54d33ebed3341abf2d07100d4a5669f2ea669db9
Joyce <joyceyukang@gmail.com>
2024-01-17 21:17:48 -0800

    transcriber with flask

--- main.py
+++ main.py
@@ -1,35 +1,53 @@
-import PySimpleGUI as sg
-import speech_recognition as sr
+from flask import Flask, render_template
+from deepgram import Deepgram
+from dotenv import load_dotenv
+import os
 
-sg.theme("DarkBlue14")
-sg.set_options(font=("Helvetica", 14))
+app = Flask(__name__)
 
-# create layout of GUI
-layout = [[sg.Text("speeach to Text")], [sg.Multiline(size=(70, 20), key="-OUTPUT-")], [sg.Button("Record", button_color=('white', 'gray'), border_width=10), sg.Button("Exit", button_color=("white", "red"), border_width=10)]]
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-window = sg.Window("Speech to Text", layout)
 
-# Loop for events
-while True:
-    event, values = window.read()
-    if event == "Exit" or event == sg.WIN_CLOSED:
-        break
-    elif event == "Record":
-        # Initialize recognizer
-        r = sr.Recognizer()
 
-        # Record Audio
-        with sr.Microphone() as source:
-            audio = r.listen(source)
+
+
+
+
+
+# import PySimpleGUI as sg
+# import speech_recognition as sr
+
+# sg.theme("DarkBlue14")
+# sg.set_options(font=("Helvetica", 14))
+
+# # create layout of GUI
+# layout = [[sg.Text("speeach to Text")], [sg.Multiline(size=(70, 20), key="-OUTPUT-")], [sg.Button("Record", button_color=('white', 'gray'), border_width=10), sg.Button("Exit", button_color=("white", "red"), border_width=10)]]
+
+# window = sg.Window("Speech to Text", layout)
+
+# # Loop for events
+# while True:
+#     event, values = window.read()
+#     if event == "Exit" or event == sg.WIN_CLOSED:
+#         break
+#     elif event == "Record":
+#         # Initialize recognizer
+#         r = sr.Recognizer()
+
+#         # Record Audio
+#         with sr.Microphone() as source:
+#             audio = r.listen(source)
         
-        # Recognize speech using Google Search recognition
-        try:
-            text = r.recognize.google(audio)
-            window["-OUTPUT-"].update(text)
-        except sr.UnknownValueError:
-            window["-OUTPUT-"].update("Could not understand audio")
-        except sr.RequestError as e:
-            window["-OUTPUT-"].update("Error: {e}")
+#         # Recognize speech using Google Search recognition
+#         try:
+#             text = r.recognize.google(audio)
+#             window["-OUTPUT-"].update(text)
+#         except sr.UnknownValueError:
+#             window["-OUTPUT-"].update("Could not understand audio")
+#         except sr.RequestError as e:
+#             window["-OUTPUT-"].update("Error: {e}")
 
-window.close()
+# window.close()
             
